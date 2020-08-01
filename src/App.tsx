e18477cea5d9bfea12fff884b19e2ba00f93e826
Yannick Stephan <Yannick_bmx12@gmx.de>
2020-08-01 20:00:31 +0200

    Eslint, new animation

--- src/App.tsx
+++ src/App.tsx
@@ -5,40 +5,59 @@ import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons';
 
 const startText = [
   { id: 0, text: 'Development.' },
-  { id: 1, text: 'CS Student.' }
+  { id: 1, text: 'CS Student.' },
+  { id: 3, text: 'Passion.' }
 ];
 
 export const App = () => {
   const [index, set] = useState<number>(0);
   const parallax = React.useRef<Parallax>() as MutableRefObject<Parallax>;
   useEffect(
-    () => void setInterval(() => set((state) => (state + 1) % 2), 2000),
+    () =>
+      void setInterval(
+        () => set((state) => (state + 1) % startText.length),
+        5000
+      ),
     []
   );
   const transitions = useTransition(startText[index], (item) => item.id, {
-    from: { position: 'absolute', overflow: 'hidden', height: 0, opacity: 0 },
-    enter: { height: 200, opacity: 1 },
-    leave: { height: 0, opacity: 0 },
-    config: config.slow
+    from: {
+      position: 'absolute',
+      opacity: 0,
+      transform: 'translate3d(-50%,-40%,0)',
+      left: '50%',
+      top: '50%',
+      textAlign: 'center'
+    },
+    enter: { opacity: 1, transform: 'translate3d(-50%,-50%,0)' },
+    leave: { opacity: 0, transform: 'translate3d(-50%,-60%,0)' },
+    config: config.stiff
   });
 
   return (
     <div className="App">
       <div className="noise" />
       <Parallax pages={2} scrolling={true} ref={parallax}>
-        <ParallaxLayer offset={0} speed={0.5}>
-          {transitions.map(({ item, props, key }) => (
-            <animated.div key={key} style={props} className="start-headline">
-              {item.text}
-            </animated.div>
-          ))}
-          <span onClick={() => parallax.current.scrollTo(1)}>
+        <ParallaxLayer offset={0}>
+          <div className="start-headline">
+            {transitions.map(({ item, props, key }) => (
+              <animated.div key={key} style={props} className="">
+                {item.text}
+              </animated.div>
+            ))}
+          </div>
+          <div
+            className="arrow-wrap"
+            onClick={() => parallax.current.scrollTo(1)}
+          >
             <i className="arrow down"></i>
-          </span>
+          </div>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={1} speed={0.5}>
-          <span>Layer 2</span>
+        <ParallaxLayer offset={1} speed={0.4}>
+          <div style={{ height: '100%', backgroundColor: '#003633' }}>
+            <span>Layer 2</span>
+          </div>
         </ParallaxLayer>
       </Parallax>
     </div>
