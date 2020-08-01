bf95b0aa390905d8a6e9cc165411d894e9355c8a
Yannick Stephan <Yannick_bmx12@gmx.de>
2020-08-01 18:11:40 +0200

    Updates

---
+++ precache-manifest.f93963057949530f932fe19029f215ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d705f9a31ca7f915cca316c9d240f106",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "760518359d40c0ff741b",
+    "url": "/portfolio/static/css/main.2f640a7e.chunk.css"
+  },
+  {
+    "revision": "eeef79a8a18f2f266a6a",
+    "url": "/portfolio/static/js/2.f582802f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.f582802f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "760518359d40c0ff741b",
+    "url": "/portfolio/static/js/main.de0e77cd.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "1384f2dfaf177439200b5dc575780043",
+    "url": "/portfolio/static/media/bg.1384f2df.gif"
+  }
+]);
