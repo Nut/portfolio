3f20c3c41036d1dec08e6aa78c437e5157696104
Yannick Stephan <Yannick_bmx12@gmx.de>
2020-08-02 22:15:04 +0200

    Changed font

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { MutableRefObject, useEffect, useState } from 'react';
 import './App.css';
-import { animated, useTransition, config } from 'react-spring';
+import { animated, useTransition, config, useSpring } from 'react-spring';
 import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons';
 
 const startText = [
@@ -9,14 +9,14 @@ const startText = [
   { id: 3, text: 'Passion.' }
 ];
 
-export const App = () => {
+export const App = (): JSX.Element => {
   const [index, set] = useState<number>(0);
   const parallax = React.useRef<Parallax>() as MutableRefObject<Parallax>;
   useEffect(
     () =>
       void setInterval(
         () => set((state) => (state + 1) % startText.length),
-        5000
+        2000
       ),
     []
   );
@@ -27,18 +27,28 @@ export const App = () => {
       transform: 'translate3d(-50%,-40%,0)',
       left: '50%',
       top: '50%',
-      textAlign: 'center'
+      textAlign: 'center',
+      width: '100%'
     },
     enter: { opacity: 1, transform: 'translate3d(-50%,-50%,0)' },
     leave: { opacity: 0, transform: 'translate3d(-50%,-60%,0)' },
     config: config.stiff
   });
 
+  const props = useSpring({ opacity: 1, from: { opacity: 0 } });
+
   return (
     <div className="App">
       <div className="noise" />
       <Parallax pages={2} scrolling={true} ref={parallax}>
-        <ParallaxLayer offset={0}>
+        <ParallaxLayer offset={0} speed={0.5}>
+          <div className="start-headline-bg">
+            {transitions.map(({ item, props, key }) => (
+              <animated.div key={key} style={props} className="">
+                {item.text}
+              </animated.div>
+            ))}
+          </div>
           <div className="start-headline">
             {transitions.map(({ item, props, key }) => (
               <animated.div key={key} style={props} className="">
@@ -46,15 +56,17 @@ export const App = () => {
               </animated.div>
             ))}
           </div>
-          <div
-            className="arrow-wrap"
-            onClick={() => parallax.current.scrollTo(1)}
-          >
-            <i className="arrow down"></i>
-          </div>
+          <animated.div style={props}>
+            <div
+              className="arrow-wrap"
+              onClick={() => parallax.current.scrollTo(1)}
+            >
+              <i className="arrow down"></i>
+            </div>
+          </animated.div>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={1} speed={0.4}>
+        <ParallaxLayer offset={1} speed={0.5}>
           <div style={{ height: '100%', backgroundColor: '#003633' }}>
             <span>Layer 2</span>
           </div>
