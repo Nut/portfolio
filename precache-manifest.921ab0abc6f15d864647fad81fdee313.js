4ce19cac3d97a24525c355cabf0558c6a69462ce
Yannick Stephan <Yannick_bmx12@gmx.de>
2020-08-04 17:10:00 +0200

    Updates

---
+++ precache-manifest.921ab0abc6f15d864647fad81fdee313.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e210953f65de0097b80bbaf398a5960",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3792cb50082a3921643a",
+    "url": "/portfolio/static/css/main.460437ea.chunk.css"
+  },
+  {
+    "revision": "a182d7d18432aa78c9bf",
+    "url": "/portfolio/static/js/2.a780bdb4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.a780bdb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3792cb50082a3921643a",
+    "url": "/portfolio/static/js/main.0d275d8d.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "f5eea207ed1060c86a16865abd0c00f8",
+    "url": "/portfolio/static/media/OiMate.f5eea207.otf"
+  },
+  {
+    "revision": "adbdb35f73b5325ffd9c9949dcd97966",
+    "url": "/portfolio/static/media/OiYou.adbdb35f.otf"
+  },
+  {
+    "revision": "1384f2dfaf177439200b5dc575780043",
+    "url": "/portfolio/static/media/bg.1384f2df.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.aba28b8b7848da345ba0d680cb105c2b.js"
+  "/portfolio/precache-manifest.921ab0abc6f15d864647fad81fdee313.js"
 );
 
 self.addEventListener('message', (event) => {
